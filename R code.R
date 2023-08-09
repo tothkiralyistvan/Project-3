3ca5c884779bed890e091577d4bdce020c9dd494
tothkiralyistvan <tothkiralyistvan@gmail.com>
2023-08-09 19:22:27 -0400

    Update

--- R code.R
+++ R code.R
@@ -13,3 +13,6 @@ View(dataset)
 
 names(dataset)
 glimpse(dataset)  
+
+# drop columns that are definitely not needed
+removed_cols <- c("series_add", "sal2_di", "sal2_di", "tol3_di", "mood6_di", "rel7_di", "with9_di", "conf12_di", "prob14_di", "series_cutoff", "series_cutoff_di")
