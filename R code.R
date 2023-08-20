423382934273c72cd1281901121a8adaac45ca05
tothkiralyistvan <tothkiralyistvan@gmail.com>
2023-08-20 17:15:40 -0400

    Update file

--- R code.R
+++ R code.R
@@ -81,3 +81,16 @@ data_new <- data_new %>%
   select(-one_of(removed_cols))
 
 glimpse(data_new)
+
+# create item lists for alphas and descriptives
+list_surg <- c("surg1", "surg2", "surg3", "surg4", "surg5", "surg6")
+list_gyasz <- c("gyasz1", "gyasz2", "gyasz3", "gyasz4", "gyasz5", "gyasz6")
+list_teli <- c("telitodes1", "telitodes2", "telitodes3", "telitodes4", "telitodes5", "telitodes6")
+list_menny <- c("mennyiseg1", "mennyiseg2", "mennyiseg3", "mennyiseg4", "mennyiseg5", "mennyiseg6")
+list_cont <- c("kontrollvesztes1", "kontrollvesztes2", "kontrollvesztes3", "kontrollvesztes4", "kontrollvesztes5", "kontrollvesztes6")
+list_iden <- c("identifikacio1", "identifikacio2", "identifikacio3", "identifikacio4", "identifikacio5", "identifikacio6")
+list_offl <- c("offline1", "offline2", "offline3", "offline4", "offline5", "offline6", "offline7")
+list_onli <- c("online1", "online2", "online4", "online5", "online6", "online7")
+list_onfe <- c("onfejlesztes1", "onfejlesztes2", "onfejlesztes3", "onfejlesztes4", "onfejlesztes5", "onfejlesztes6")
+list_bunt <- c("buntudat1", "buntudat2", "buntudat3", "buntudat4", "buntudat5", "buntudat6")
+list_prob <- c("sal1", "sal2", "tol3", "tol4", "erz5", "erz6", "rel7", "rel8", "meg9", "meg10", "kon11", "kon12", "pro13", "pro14")
